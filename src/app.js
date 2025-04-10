cc783d35cc42f9585493813951485fb85ecc4505
yashssalvi <74598977+yashssalvi@users.noreply.github.com>
2025-04-10 21:28:51 +0530

    playing with routs

--- src/app.js
+++ src/app.js
@@ -3,26 +3,8 @@ const express = require("express");
 const app = express();
 
 app.use("/user", (req, res) => {
-    res.send("HAHAHAHAHAHAHAHAHAH!");
-});
-
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
+    console.log(req.query)
     res.send({firstname: "Yash", lastname: "Salvi"});
-});
-
-app.post("/user", (req, res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the database!");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully!");
-});
-
-// This will match all http methods API call to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server !");
 });
 
 app.listen(3000, () => {
