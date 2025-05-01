32936f17e1c94ca6bec4b653ae545ca33af95f24
yashssalvi <74598977+yashssalvi@users.noreply.github.com>
2025-05-01 11:54:29 +0530

    Exploring validator library

--- src/app.js
+++ src/app.js
@@ -62,7 +62,23 @@ app.delete("/user", async(req, res) => {
 app.patch("/user", async(req, res) => {
     const userId = req.body.userId;
     const data = req.body;
+
     try{
+        
+        const ALLOWED_UPDATE = ["userId", "photoUrl", "about", "gender", "age" ,"skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k) => 
+            ALLOWED_UPDATE.includes(k)
+        );
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        };
+
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        };
+
         const user = await User.findByIdAndUpdate({ _id: userId}, data, {
             returnDocument: "after",
             runValidators: true,
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,11 +16,21 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address!");
+            };
+        }
     },
     password: {
         type: String,
-        required: true,
+        required: true,        
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter Strong Password");
+            };
+        },
     },
     age: {
         type: Number,
@@ -35,7 +46,12 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl: {
         type: String,
-        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwOVBmhdc2jcsiq5rULhg2UF06-0_2pq3-4g&s"
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwOVBmhdc2jcsiq5rULhg2UF06-0_2pq3-4g&s",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL!");
+            };
+        }
     },
     about: {
         type: String,
